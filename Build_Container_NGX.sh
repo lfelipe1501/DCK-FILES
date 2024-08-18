0bb3bded2689feded2b16e529c6acaff9b0e9fb6
Luis Felipe Sanchez <lfelipe1501@gmail.com>
2024-08-18 01:30:10 -0500

    Docker LEMP stack 2.0

---
+++ Build_Container_NGX.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#
+# Script to Generate lfsys CONTAINER for LEMP Stack.
+#
+# @author   Luis Felipe <lfelipe1501@gmail.com>
+# @website  https://www.lfsystems.com.co
+# @version  1.0
+
+echo "Generando Contenedor de NGINX + UI"
+echo ""
+docker buildx build --push --platform linux/arm64,linux/amd64 -t lfsys/nginx:latest DCK-FILES/GenerateNGINX/.
+echo ""
+
+echo ""
+echo "Todo listo =)"
+echo ""
---
+++ Build_Containers_PHP.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#
+# Script to Generate lfsys CONTAINERs for LEMP Stack.
+#
+# @author   Luis Felipe <lfelipe1501@gmail.com>
+# @website  https://www.lfsystems.com.co
+# @version  1.0
+
+echo "Generando Contenedor de PHP 7.4"
+echo ""
+docker build --builder cloud-lfsys-lfsystems --push --platform linux/arm64,linux/amd64 -t lfsys/php74:latest DCK-FILES/GeneratePHP7/.
+echo ""
+
+echo "Generando Contenedor de PHP 8"
+echo ""
+docker build --builder cloud-lfsys-lfsystems --push --platform linux/arm64,linux/amd64 -t lfsys/php:latest DCK-FILES/GeneratePHP8/.
+echo ""
+
+echo ""
+echo "Todo listo =)"
+echo ""
